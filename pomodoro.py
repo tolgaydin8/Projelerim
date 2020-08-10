3f958c057d18e52e5d9cf4f96932f4455e790b3a
tolgaydin8 <67922344+tolgaydin8@users.noreply.github.com>
2020-08-10 16:22:12 +0300

    Add files via upload

---
+++ pomodoro.py
@@ -0,0 +1,106 @@
+import time
+import os
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
+
+def uyari_kutusu():
+    messagebox.showinfo("UYARI!","""Pomodoron bitti dinlenme vakti
+    Hazır olunca Tamam'a bas.""")
+def uyari_kutusu2():
+    messagebox.showinfo("UYARI!","""Mola bitti pomodoraya devam
+    Hazır olunca Tamam'a bas """)
+    
+
+def pomodoro_25_dk(): # 25 dk pomodoro ve molanın fonksiyonu
+    for i in range(26):
+        print(str(25-i)+ " dakika kaldı")
+        time.sleep(1)
+        os.system("cls")
+        if i == 25:
+            uyari_kutusu()
+
+def pomodoro_kısa_molda_5_dk():
+    for j in range (6):
+        print("Molanızın bitmesine "+ str(5-j) + " dakika kaldı")
+        time.sleep(1)
+        os.system("cls")
+        if j == 5:
+            uyari_kutusu2()
+
+
+
+def pomodoro_uzun_mola_15_dk():
+    for j in range (15):
+        print("Molanızın bitmesine "+ str(15-j) + " dakika kaldı")
+        time.sleep(1)
+        os.system("cls")
+        if j == 15:
+            uyari_kutusu2()
+
+
+def pomodoro_custom_ders(): 
+    for i in range(ders_süresi + 1 ):
+        print(str(ders_süresi - i)+ " dakika kaldı")
+        time.sleep(1)
+        os.system("cls")
+        if i == ders_süresi:
+            uyari_kutusu()
+
+
+def pomodoro_custom_kısa_mola():
+    for j in range (mola_süresi + 1):
+        print("Molanızın bitmesine "+ str(mola_süresi-j) + " dakika kaldı")
+        time.sleep(1)
+        os.system("cls")
+        if j == mola_süresi:
+            uyari_kutusu2()
+
+
+
+def pomodoro_uzun_mola_custom():
+    for j in range (uzun_süresi+1):
+        print("Molanızın bitmesine "+ str(uzun_süresi - j) + " dakika kaldı")
+        time.sleep(1)
+        os.system("cls")
+        if j == uzun_süresi:
+            uyari_kutusu2()
+
+
+
+print("""Merhaba Pomodoro Zamanlayıcısına Hoşgeldin :)
+Nasıl bir pomodoro istediğini tuşar mısın? 
+1- Klasik pomodoro (25-5-15-4)
+2-Custom""")
+sayı = int(input(" "))
+try:
+    if sayı == 1:
+        for j in range(4):
+            pomodoro_25_dk()
+            pomodoro_kısa_molda_5_dk()
+            pomodoro_25_dk()
+            pomodoro_kısa_molda_5_dk()
+            pomodoro_25_dk()                      #1. Pomodoro dönügüsü
+            pomodoro_kısa_molda_5_dk() 
+            pomodoro_25_dk()
+            pomodoro_uzun_mola_15_dk()
+        messagebox.showinfo("UYARI!","""Pomodoron bitti tebrikler!!.""")
+
+    elif sayı == 2 :
+        ders_süresi = int(input("ders süresini dakika cinsinen tuşlayınız: "))
+        mola_süresi = int(input("Mola süresini dakika cinsinden tuşlayınız: "))
+        uzun_süresi = int(input("uzun molda süresini dakika cinsinden tuşlayınız: "))
+        tekrar = int (input("tekrar sayısı tuşlayınız: "))
+
+    for i in range(tekrar):
+        pomodoro_custom_ders()
+        pomodoro_custom_kısa_mola()
+        pomodoro_custom_ders()
+        pomodoro_custom_kısa_mola()
+        pomodoro_custom_ders()
+        pomodoro_custom_kısa_mola()
+        pomodoro_custom_ders()
+        pomodoro_uzun_mola_custom()
+except:
+    print("Lütfen düzgün bir değer giriniz")
---
+++ şifre_üretici.py
@@ -0,0 +1,32 @@
+import string
+import random
+
+if __name__ == "__main__":
+    harfler = string.ascii_letters # Büyüklü küçüklü harfler 
+    rakamlar = string.digits #0123456789
+    bazi_semboller = ["!","?","%","/","&","<",">",".",",","_","=","+","-","*",")","("]
+    liste = []
+    liste.extend(list(harfler))
+    liste.extend(list(rakamlar))
+    liste.extend(bazi_semboller)
+    def random_sifre():    
+        uzunluk = abs(int(input("Lütfen şifrenin uzunluğunu giriniz: "))) 
+        while uzunluk < 8:
+            print("lütfen 8'den fazla sayı giriniz")
+            uzunluk =abs(int(input("Lütfen şifrenin uzunluğunu giriniz: ")))
+            
+            continue
+        else:
+            random.shuffle(liste)
+            print("Şifeniz: "+ "".join(liste[0:uzunluk]))
+    
+    try:
+        random_sifre()
+    except ValueError:
+        print("lütfen sayı giriniz")
+        random_sifre()
+    
+        
+
+
+
